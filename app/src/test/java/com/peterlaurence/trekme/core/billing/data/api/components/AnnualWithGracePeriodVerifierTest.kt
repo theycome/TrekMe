9dd4a1e62536529b07c2335727e8dfd7e97238fe
Ivan Yakushev <theycome@mail.ru>
2024-10-17 19:00:02 +0300

    Add tests for checkTime Granted

--- app/src/test/java/com/peterlaurence/trekme/core/billing/data/api/components/AnnualWithGracePeriodVerifierTest.kt
+++ app/src/test/java/com/peterlaurence/trekme/core/billing/data/api/components/AnnualWithGracePeriodVerifierTest.kt
@@ -4,6 +4,7 @@ import android.text.format.DateUtils
 import com.peterlaurence.trekme.core.billing.domain.model.AccessGranted
 import com.peterlaurence.trekme.util.datetime.Millis
 import com.peterlaurence.trekme.util.datetime.days_
+import com.peterlaurence.trekme.util.datetime.int
 import com.peterlaurence.trekme.util.datetime.millis
 import com.peterlaurence.trekme.util.randomizers.IntRangeRandomizer
 import io.kotest.assertions.withClue
@@ -51,15 +52,16 @@ class AnnualWithGracePeriodVerifierTest {
 
         val randomizer = IntRangeRandomizer(7, 0..Int.MAX_VALUE)
 
-        randomizer.forEach { (a, _) ->
-            withClue("$a") {
-                val daysInMillis = a / DateUtils.DAY_IN_MILLIS.toInt()
+        randomizer.forEach { (it, _) ->
+            withClue("$it") {
+
+                val days = it / DateUtils.DAY_IN_MILLIS.toInt()
 
                 verifier.checkTime(
-                    now + (a).millis,
+                    now + (it).millis,
                     now
                 ) shouldBe
-                    AccessGranted(validityDuration + daysInMillis)
+                    AccessGranted(validityDuration + days)
 
             }
         }
@@ -69,8 +71,48 @@ class AnnualWithGracePeriodVerifierTest {
     @Test
     fun `checkTime Granted`() {
 
-//        verifier.checkTime(now + 1.millis, now) shouldBe
-//            AccessGranted(validityDuration)
+        verifier.checkTime(now - 1.millis, now) shouldBe
+            AccessGranted(validityDuration)
+
+        verifier.checkTime(now - 1.days_.millis, now) shouldBe
+            AccessGranted(validityDuration - 1)
+
+        verifier.checkTime(now - 115.days_.millis, now) shouldBe
+            AccessGranted(validityDuration - 115)
+
+        verifier.checkTime(
+            now - (115.days_.millis + 1.days_.millis + 1.millis),
+            now
+        ) shouldBe
+            AccessGranted(validityDuration - 115 - 1)
+
+        verifier.checkTime(
+            now - (115.days_.millis + 1.days_.millis),
+            now
+        ) shouldBe
+            AccessGranted(validityDuration - 115 - 1)
+
+    }
+
+
+    @Test
+    fun `checkTime Granted random`() {
+
+        val randomizer = IntRangeRandomizer(7, 0..validityDuration)
+
+        randomizer.forEach { (it, _) ->
+            withClue("$it") {
+
+                val days = it.days_
+
+                verifier.checkTime(
+                    now - days.millis,
+                    now
+                ) shouldBe
+                    AccessGranted(validityDuration - days.int)
+
+            }
+        }
 
     }
 
