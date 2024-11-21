2c91424fa067e69fd161e8b6e678721bff11c15b
Ivan Yakushev <theycome@mail.ru>
2024-11-21 18:52:52 +0300

    Add TODO comment

--- app/src/main/java/com/peterlaurence/trekme/util/Any.kt
+++ app/src/main/java/com/peterlaurence/trekme/util/Any.kt
@@ -5,5 +5,6 @@ import android.util.Log
 /**
  * Created by Ivan Yakushev on 17.11.2024
  */
+// TODO - enhance with clickable file-line link
 fun Any.log(objekt: Any) =
     Log.i(this::class.simpleName, "$objekt - [${objekt::class.qualifiedName}]")
