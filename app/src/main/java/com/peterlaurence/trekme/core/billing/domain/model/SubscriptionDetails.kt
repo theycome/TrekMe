7e97f17fab2424a059ba2c7fb5f5105380e3e453
Ivan Yakushev <theycome@mail.ru>
2024-11-27 16:22:36 +0300

    Move TrialInfo into its own file

--- app/src/main/java/com/peterlaurence/trekme/core/billing/domain/model/SubscriptionDetails.kt
+++ app/src/main/java/com/peterlaurence/trekme/core/billing/domain/model/SubscriptionDetails.kt
@@ -1,7 +1,5 @@
 package com.peterlaurence.trekme.core.billing.domain.model
 
-import com.peterlaurence.trekme.util.datetime.Days
-import com.peterlaurence.trekme.util.datetime.days_
 import java.util.UUID
 
 data class SubscriptionDetails(
@@ -9,44 +7,3 @@ data class SubscriptionDetails(
     val price: String,
     val trialInfo: TrialInfo,
 )
-
-sealed interface TrialInfo
-
-data class TrialAvailable(val duration: Days) : TrialInfo {
-
-    companion object {
-
-        /**
-         * Trial periods are given in the form of "P1W" -> 1 week, or "P4D" -> 4 days.
-         */
-        operator fun invoke(period: String): TrialAvailable? {
-
-            if (period.isEmpty()) return TrialAvailable(0.days_)
-            val lowercased = period.lowercase()
-
-            val days = runCatching {
-                if (lowercased.first() != 'p') return@runCatching null
-
-                val quantity = lowercased.filter { it.isDigit() }.toInt()
-                val lastChar = lowercased.last()
-
-                when (lastChar) {
-                    'w' -> quantity * DAYS_IN_WEEK
-                    'd' -> quantity
-                    else -> null
-                }?.days_
-
-            }.getOrNull()
-
-            return if (days != null) {
-                TrialAvailable(days)
-            } else null
-        }
-
-        private const val DAYS_IN_WEEK: Int = 7
-
-    }
-
-}
-
-data object TrialUnavailable : TrialInfo
---
+++ app/src/main/java/com/peterlaurence/trekme/core/billing/domain/model/TrialInfo.kt
@@ -0,0 +1,48 @@
+package com.peterlaurence.trekme.core.billing.domain.model
+
+import com.peterlaurence.trekme.util.datetime.Days
+import com.peterlaurence.trekme.util.datetime.days_
+
+/**
+ * Created by Ivan Yakushev on 27.11.2024
+ */
+sealed interface TrialInfo
+
+data class TrialAvailable(val duration: Days) : TrialInfo {
+
+    companion object {
+
+        /**
+         * Trial periods are given in the form of "P1W" -> 1 week, or "P4D" -> 4 days.
+         */
+        operator fun invoke(period: String): TrialAvailable? {
+
+            if (period.isEmpty()) return TrialAvailable(0.days_)
+            val lowercased = period.lowercase()
+
+            val days = runCatching {
+                if (lowercased.first() != 'p') return@runCatching null
+
+                val quantity = lowercased.filter { it.isDigit() }.toInt()
+                val lastChar = lowercased.last()
+
+                when (lastChar) {
+                    'w' -> quantity * DAYS_IN_WEEK
+                    'd' -> quantity
+                    else -> null
+                }?.days_
+
+            }.getOrNull()
+
+            return if (days != null) {
+                TrialAvailable(days)
+            } else null
+        }
+
+        private const val DAYS_IN_WEEK: Int = 7
+
+    }
+
+}
+
+data object TrialUnavailable : TrialInfo
