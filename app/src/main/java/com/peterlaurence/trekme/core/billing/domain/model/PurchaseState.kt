8dc4c8b0c003980cb9e382868e502a09a24bc185
Ivan Yakushev <theycome@mail.ru>
2024-12-31 15:41:29 +0300

    Reformat code

--- app/src/main/java/com/peterlaurence/trekme/core/billing/domain/model/PurchaseState.kt
+++ app/src/main/java/com/peterlaurence/trekme/core/billing/domain/model/PurchaseState.kt
@@ -1,5 +1,9 @@
 package com.peterlaurence.trekme.core.billing.domain.model
 
 enum class PurchaseState {
-    CHECK_PENDING, PURCHASED, NOT_PURCHASED, PURCHASE_PENDING, UNKNOWN
+    CHECK_PENDING,
+    PURCHASED,
+    NOT_PURCHASED,
+    PURCHASE_PENDING,
+    UNKNOWN,
 }
