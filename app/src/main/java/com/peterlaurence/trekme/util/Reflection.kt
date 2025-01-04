296109e8f7bd6566c93f14248cd7fdf681d7e9cc
Ivan Yakushev <theycome@mail.ru>
2025-01-05 01:57:57 +0300

    Add utility function for getting members of data class

---
+++ app/src/main/java/com/peterlaurence/trekme/util/Reflection.kt
@@ -0,0 +1,23 @@
+package com.peterlaurence.trekme.util
+
+/**
+ * Created by Ivan Yakushev on 31.12.2024
+ */
+/**
+ * Allows to obtain an iterable collection of object instances
+ * representing all values present in some sealed class/interface hierarchy
+ * ```
+ * sealed interface MonthAndYear : SubscriptionType {
+ *     data object Month : MonthAndYear
+ *     data object Year : MonthAndYear
+ * }
+ *
+ * val list: List<SubscriptionType.MonthAndYear> = nestedClassesInstances<SubscriptionType.MonthAndYear>()
+ * ```
+ */
+inline fun <reified T> sealedSubclassesInstances(): List<T> =
+    T::class.sealedSubclasses.map {
+        it.objectInstance as T
+    }.run {
+        ifEmpty { listOf(T::class.objectInstance as T) }
+    }
